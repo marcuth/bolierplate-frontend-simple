b90bc0b065d2ad9d7ba58e344e116e3d6866c183
Marcuth <thiago.playgames15@gmail.com>
2025-09-10 18:44:42 -0300

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,8 @@ import { ThemeProvider } from "next-themes"
 import { cookies } from "next/headers"
 import { FC, ReactNode } from "react"
 
-import { SidebarProvider, SidebarTrigger } from "../components/ui/sidebar"
-import AppSidebar from "../components/layout/application/app-sidebar"
+import AppSidebar from "@/components/layout/application/app-sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar"
 
 type Props = {
     children: ReactNode
@@ -17,15 +17,7 @@ const Providers: FC<Props> = async ({ children }) => {
         <ThemeProvider attribute="class" enableSystem={true} defaultTheme="system" themes={["light", "dark"]}>
             <SidebarProvider defaultOpen={defaultOpen}>
                 <AppSidebar />
-                <main>
-                    <div className="h-16 flex items-center justify-between px-4">
-                        <div className="flex items-center space-x-2">
-                            <SidebarTrigger />
-                            <span className="text-xs">Menu</span>
-                        </div>
-                    </div>
-                    <div className="px-4">{children}</div>
-                </main>
+                {children}
             </SidebarProvider>
         </ThemeProvider>
     )
